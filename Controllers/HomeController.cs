2d801d13b7ee029ed3cea3b082659e91b4028d41
florianwrmbtr <florianwermbter@gmail.com>
2024-01-02 23:26:04 +0100

    New Userinterface

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,8 +12,6 @@ namespace smartstall.Controllers
     public class HomeController : Controller
     {
         Ammoniak amonniak = new Ammoniak();
-        Luftfeuchtigkeit luftfeuchtigkeit = new Luftfeuchtigkeit();
-        Temperatur temperatur = new Temperatur();
         private readonly DbReadService _readService;
 
         public HomeController(DbReadService readService)
@@ -23,80 +21,167 @@ namespace smartstall.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.ammoniakWerteTagAktuell = amonniak.AmmoniakAktuellListeTag;
-            ViewBag.aktuellerWertAmmoniak = amonniak.AktuellerWertAmmoniak;
+            //ViewBag.ammoniakWerteTagAktuell = amonniak.AmmoniakAktuellListeTag;
+            //ViewBag.aktuellerWertAmmoniak = amonniak.AktuellerWertAmmoniak;
 
-            ViewBag.luftfeuchtigkeitWerteTagAktuell = luftfeuchtigkeit.LuftfeuchtigkeitAktuellListeTag;
-            ViewBag.aktuellerWertLuftfeuchtigkeit = luftfeuchtigkeit.AktuellerWertLuftfeuchtigkeit;
-
-            ViewBag.temperaturWerteTagAktuell = temperatur.TemperaturAktuellListeTag;
-            ViewBag.aktuellerWertTemperatur = temperatur.AktuellerWertTemperatur;
+            ViewBag.test = getAmmoniakFunction();
 
             return View();
         }
 
         public ActionResult GetAmmoniak()
         {
-            //Dictionary<string, string> ammoniakWerte = new Dictionary<string, string>();                       
+            return getAmmoniakFunction();
+        }
 
-            ////get Dictionary with all times and ammoniak values for a speciffic day
-            //// "ammoniak" can be replaced with "luftfeuchtigkeit" and "temperatur" for the other diagrams
-            //Dictionary<DateTime, string> ammoniakTag = _readService.GetDataForDay(DateTime.Now, "ammoniak");
+        public ActionResult GetLuftfeuchtigkeit()
+        {
+
+            Dictionary<string, Dictionary<DateTime, string>> luftfeuchtigkeitWerte = new Dictionary<string, Dictionary<DateTime, string>>();
+
+            //get Dictionary with all times and ammoniak values for a speciffic day
+            // "ammoniak" can be replaced with "luftfeuchtigkeit" and "temperatur" for the other diagrams
+            DateTime i = DateTime.Parse("2023-06-15 14:05:47");
+            Dictionary<DateTime, string> luftfeuchtigkeitTag = _readService.GetDataForDay(i, "luftfeuchtigkeit");
+
             //List<string> ammoniakValuesDay = new List<string>();
             //foreach (string ammoniakValueDay in ammoniakTag.Values)
             //{
             //    ammoniakValuesDay.Add(ammoniakValueDay);
             //}
-            ////create full string to replace "amonniak.AmmoniakAktuellListeTag" with
+            //create full string to replace "amonniak.AmmoniakAktuellListeTag" with
             //string ammoniakStringDay = "[" + string.Join(", ", ammoniakValuesDay) + "]";
-            ////ammoniakWerte.Add("ammoniakWerteTagAktuell", amonniak.AmmoniakAktuellListeTag);
-            //ammoniakWerte.Add("ammoniakWerteTagAktuell", ammoniakStringDay);                        
-            ////todo: average values
-            //ammoniakWerte.Add("ammoniakWerteTagDurchschnitt", amonniak.AmmoniakDurschnittListeTag);
+            //ammoniakWerte.Add("ammoniakWerteTagAktuell", amonniak.AmmoniakAktuellListeTag);
+            luftfeuchtigkeitWerte.Add("luftfeuchtigkeitWerteTagAktuell", luftfeuchtigkeitTag);
+            //todo: average values
+            luftfeuchtigkeitWerte.Add("luftfeuchtigkeitkWerteTagDurchschnitt", null);
 
 
-            ////same thing for week (dictionary contains all data from last 7 days)
-            //Dictionary<DateTime, string> ammoniakWoche = _readService.GetDataForWeek(DateTime.Now, "ammoniak");            
+            //same thing for week (dictionary contains all data from last 7 days)
+            //Dictionary<DateTime, string> luftfeuchtigkeitWoche = _readService.GetDataForWeek(DateTime.Now, "luftfeuchtigkeit");
+
+
+            Dictionary<DateTime, string> luftfeuchtigkeitWoche = funktion();
+
+
             //List<string> ammoniakValuesWeek = new List<string>();
             //foreach (string ammoniakValue in ammoniakWoche.Values)
             //{
             //    ammoniakValuesWeek.Add(ammoniakValue);
-            //}            
+            //}
             //string ammoniakStringWeek = "[" + string.Join(", ", ammoniakValuesWeek) + "]";
-            ////ammoniakWerte.Add("ammoiakWerteWocheAktuell", amonniak.AmmoniakAktuellWoche);
-            //ammoniakWerte.Add("ammoniakWerteWocheAktuell", ammoniakStringWeek);
-            ////todo: average values
-            //ammoniakWerte.Add("ammoniakWerteWocheDurchschnitt", amonniak.AmmoniakDurchschnittWoche);
-            //return Json(ammoniakWerte);
-
-            // Änderungen Hannes
-
-            Dictionary<string, string> ammoniakData = new Dictionary<string, string>();
-            ammoniakData.Add("AmmoniakAktuellListeTag", amonniak.AmmoniakAktuellListeTag);
-            ammoniakData.Add("AmmoniakAktuellListeWoche", amonniak.AmmoniakAktuellListeWoche);
-            ammoniakData.Add("aktuellerWertAmmnoniak", amonniak.AktuellerWertAmmoniak);
-
-            return Json(ammoniakData);
-        }
-
-        public ActionResult GetLuftfeuchtigkeit()
-        {
-            Dictionary<string, string> luftfeuchtigkeitWerte = new Dictionary<string, string>();
-            luftfeuchtigkeitWerte.Add("luftfeuchtigkeitWerteTagAktuell", luftfeuchtigkeit.LuftfeuchtigkeitAktuellListeTag);
-            luftfeuchtigkeitWerte.Add("luftfeuchtigkeitWerteWocheAktuell", luftfeuchtigkeit.LuftfeuchtigkeitAktuellWoche);
-            luftfeuchtigkeitWerte.Add("aktuellerWertLuftfeuchtigkeit", luftfeuchtigkeit.AktuellerWertLuftfeuchtigkeit);
-
+            //ammoniakWerte.Add("ammoiakWerteWocheAktuell", amonniak.AmmoniakAktuellWoche);
+            luftfeuchtigkeitWerte.Add("luftfeuchtigkeitWerteWocheAktuell", luftfeuchtigkeitWoche);
+            //todo: average values
+            luftfeuchtigkeitWerte.Add("luftfeuchtigkeitWerteWocheDurchschnitt", null);
             return Json(luftfeuchtigkeitWerte);
         }
 
         public ActionResult GetTemperatur()
         {
-            Dictionary<string, string> temperaturWerte = new Dictionary<string, string>();
-            temperaturWerte.Add("temperaturWerteTagAktuell", temperatur.TemperaturAktuellListeTag);
-            temperaturWerte.Add("temperaturWerteWocheAktuell", temperatur.TemperaturAktuellWoche);
-            temperaturWerte.Add("aktuellerWertTemperatur", temperatur.AktuellerWertTemperatur);
+            Dictionary<string, Dictionary<DateTime, string>> temperaturWerte = new Dictionary<string, Dictionary<DateTime, string>>();
 
+            //get Dictionary with all times and ammoniak values for a speciffic day
+            // "ammoniak" can be replaced with "luftfeuchtigkeit" and "temperatur" for the other diagrams
+            DateTime i = DateTime.Parse("2023-06-15 14:05:47");
+            Dictionary<DateTime, string> temperaturTag = _readService.GetDataForDay(i, "temperatur");
+
+            //List<string> ammoniakValuesDay = new List<string>();
+            //foreach (string ammoniakValueDay in ammoniakTag.Values)
+            //{
+            //    ammoniakValuesDay.Add(ammoniakValueDay);
+            //}
+            //create full string to replace "amonniak.AmmoniakAktuellListeTag" with
+            //string ammoniakStringDay = "[" + string.Join(", ", ammoniakValuesDay) + "]";
+            //ammoniakWerte.Add("ammoniakWerteTagAktuell", amonniak.AmmoniakAktuellListeTag);
+            temperaturWerte.Add("temperaturWerteTagAktuell", temperaturTag);
+            //todo: average values
+            temperaturWerte.Add("temperaturWerteTagDurchschnitt", null);
+
+
+            //same thing for week (dictionary contains all data from last 7 days)
+            //Dictionary<DateTime, string> ammoniakWoche = _readService.GetDataForWeek(DateTime.Now, "ammoniak");
+
+
+            Dictionary<DateTime, string> temperaturWoche = funktion();
+
+
+            //List<string> ammoniakValuesWeek = new List<string>();
+            //foreach (string ammoniakValue in ammoniakWoche.Values)
+            //{
+            //    ammoniakValuesWeek.Add(ammoniakValue);
+            //}
+            //string ammoniakStringWeek = "[" + string.Join(", ", ammoniakValuesWeek) + "]";
+            //ammoniakWerte.Add("ammoiakWerteWocheAktuell", amonniak.AmmoniakAktuellWoche);
+            temperaturWerte.Add("temperaturWerteWocheAktuell", temperaturWoche);
+            //todo: average values
+            temperaturWerte.Add("temperaturWerteWocheDurchschnitt", null);
             return Json(temperaturWerte);
+        }
+
+        public JsonResult getAmmoniakFunction()
+        {
+            Dictionary<string, Dictionary<DateTime, string>> ammoniakWerte = new Dictionary<string, Dictionary<DateTime, string>>();
+
+            //get Dictionary with all times and ammoniak values for a speciffic day
+            // "ammoniak" can be replaced with "luftfeuchtigkeit" and "temperatur" for the other diagrams
+            DateTime i = DateTime.Parse("2023-06-15 14:05:47");
+            Dictionary<DateTime, string> ammoniakTag = _readService.GetDataForDay(i, "ammoniak");
+
+            //List<string> ammoniakValuesDay = new List<string>();
+            //foreach (string ammoniakValueDay in ammoniakTag.Values)
+            //{
+            //    ammoniakValuesDay.Add(ammoniakValueDay);
+            //}
+            //create full string to replace "amonniak.AmmoniakAktuellListeTag" with
+            //string ammoniakStringDay = "[" + string.Join(", ", ammoniakValuesDay) + "]";
+            //ammoniakWerte.Add("ammoniakWerteTagAktuell", amonniak.AmmoniakAktuellListeTag);
+            ammoniakWerte.Add("ammoniakWerteTagAktuell", ammoniakTag);
+            //todo: average values
+            ammoniakWerte.Add("ammoniakWerteTagDurchschnitt", null);
+
+
+            //same thing for week (dictionary contains all data from last 7 days)
+            //Dictionary<DateTime, string> ammoniakWoche = _readService.GetDataForWeek(DateTime.Now, "ammoniak");
+
+
+            Dictionary<DateTime, string> ammoniakWoche = funktion();
+
+
+            //List<string> ammoniakValuesWeek = new List<string>();
+            //foreach (string ammoniakValue in ammoniakWoche.Values)
+            //{
+            //    ammoniakValuesWeek.Add(ammoniakValue);
+            //}
+            //string ammoniakStringWeek = "[" + string.Join(", ", ammoniakValuesWeek) + "]";
+            //ammoniakWerte.Add("ammoiakWerteWocheAktuell", amonniak.AmmoniakAktuellWoche);
+            ammoniakWerte.Add("ammoniakWerteWocheAktuell", ammoniakWoche);
+            //todo: average values
+            ammoniakWerte.Add("ammoniakWerteWocheDurchschnitt", null);
+            return Json(ammoniakWerte);
+
+            // Änderungen Hannes
+        }
+
+        public Dictionary<DateTime, string> funktion()
+        {
+            Dictionary<DateTime, string> ammoniak = new Dictionary<DateTime, string>();
+
+            ammoniak.Add(new DateTime(2023, 12, 22, 20, 58, 00), "20");
+            ammoniak.Add(new DateTime(2023, 12, 23, 10, 58, 00), "23");
+            ammoniak.Add(new DateTime(2023, 12, 23, 20, 58, 00), "30");
+            ammoniak.Add(new DateTime(2023, 12, 24, 05, 58, 00), "28");
+            ammoniak.Add(new DateTime(2023, 12, 24, 10, 58, 00), "10");
+            ammoniak.Add(new DateTime(2023, 12, 26, 10, 58, 00), "10");
+            ammoniak.Add(new DateTime(2023, 12, 26, 15, 58, 00), "10");
+            ammoniak.Add(new DateTime(2023, 12, 26, 20, 58, 00), "15");
+            ammoniak.Add(new DateTime(2023, 12, 27, 10, 58, 00), "25");
+            ammoniak.Add(new DateTime(2023, 12, 27, 20, 58, 00), "28");
+            ammoniak.Add(new DateTime(2023, 12, 28, 10, 58, 00), "20");
+            ammoniak.Add(new DateTime(2023, 12, 28, 15, 58, 00), "20");
+            ammoniak.Add(new DateTime(2023, 12, 28, 20, 58, 00), "28");
+
+            return ammoniak;
         }
     }
 }
--- Models/Ammoniak.cs
+++ Models/Ammoniak.cs
@@ -1,18 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace smartstall.Models
+﻿namespace smartstall.Models
 {
     public class Ammoniak
     {
         public string AmmoniakAktuellListeTag { get; set; }
         public string AmmoniakAktuellListeWoche { get; set; }
-        public string AktuellerWertAmmoniak{ get; set; }
-
-        public Ammoniak()
-        {
-            AmmoniakAktuellListeTag = "[{ \"x\": 0.844, \"y\": 12 }, { \"x\": 1.124, \"y\": 19.75 }, { \"x\": 6.78, \"y\": 1 }, { \"x\": 6.8, \"y\": 1 }, { \"x\": 6.9, \"y\": 3 }, { \"x\": 7.1, \"y\": 5 }, { \"x\": 7.2, \"y\": 6 }, { \"x\": 15.78, \"y\": 14 }]";
-            AmmoniakAktuellListeWoche = "[{ \"x\": 1.124, \"y\": 24.78 }, { \"x\": 1.543, \"y\": 17.75 }, { \"x\": 2.453, \"y\": 15.75 }, { \"x\": 5.124, \"y\": 13.75 }, { \"x\": 6.78, \"y\": 1 }, { \"x\": 6.8, \"y\": 1 }, { \"x\": 6.9, \"y\": 3 }]";
-            AktuellerWertAmmoniak = "24";
-        }
+        public string AktuellerWertAmmoniak { get; set; }
     }
 }
